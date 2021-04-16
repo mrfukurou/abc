5981c281f20ae8ff207f1f864f03af069626dd65
mrfukurou <fukurou67@gmail.com>
2021-04-16 19:23:46 +0300

    4

--- abc/Program.cs
+++ abc/Program.cs
@@ -11,7 +11,7 @@ namespace abc
         static void Main(string[] args)
         {
             int a = Convert.ToInt32(Console.ReadLine());
-            int c = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
